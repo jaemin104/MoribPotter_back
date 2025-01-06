0f6621aee7bc877bd26aeee429884f83210982bb
hahachw <hahachw@kaist.ac.kr>
2025-01-06 04:07:21 +0000

    db connect

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const axios = require('axios');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
 const app = express();
 
 const KAKAO_CLIENT_ID = '9943d240d80a6557fa24722f4cff4047'; // REST API 키
-const REDIRECT_URI = 'http://localhost:3000/auth/kakao/callback';
+const REDIRECT_URI = 'http://172.10.7.89/auth/kakao/callback';
+
+// Body parser 추가 (JSON 데이터 처리)
+app.use(bodyParser.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'kakao_users',
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('MySQL 연결 오류:', err);
+        return;
+    }
+    console.log('MySQL에 연결되었습니다.');
+});
 
 // 카카오 인증 URL로 리다이렉트
 app.get('/auth/kakao', (req, res) => {
@@ -11,7 +30,7 @@ app.get('/auth/kakao', (req, res) => {
     res.redirect(kakaoAuthUrl);
 });
 
-// 카카오 인증 콜백 처리
+// 카카오 인증 콜백 처리 (로그인 및 닉네임 저장 로직 통합)
 app.get('/auth/kakao/callback', async (req, res) => {
     const code = req.query.code;
 
@@ -38,11 +57,23 @@ app.get('/auth/kakao/callback', async (req, res) => {
         });
 
         const user = userResponse.data;
+        const nickname = user.properties.nickname;
 
-        res.json({
-            accessToken: access_token,
-            email: user.kakao_account.email,
-            nickname: user.properties.nickname,
+        // MySQL에 닉네임 저장
+        const query = 'INSERT INTO users (nickname) VALUES (?) ON DUPLICATE KEY UPDATE nickname = VALUES(nickname)';
+        db.query(query, [nickname], (err, results) => {
+            if (err) {
+                console.error('MySQL 데이터 저장 오류:', err);
+                return res.status(500).send('데이터베이스 오류');
+            }
+            console.log('MySQL에 닉네임 저장 완료:', results);
+
+            // 성공 응답 반환
+            res.json({
+                accessToken: access_token,
+                nickname: nickname,
+                message: '카카오 로그인 및 닉네임 저장 성공',
+            });
         });
     } catch (error) {
         console.error(error);
@@ -50,6 +81,12 @@ app.get('/auth/kakao/callback', async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+/*
+app.get('/test', (req, res) => {
+    res.status(200).send('서버 연결 성공!');
+});
+*/
+
+app.listen(80, () => {
+    console.log('Server running on http://172.10.7.89');
 });
